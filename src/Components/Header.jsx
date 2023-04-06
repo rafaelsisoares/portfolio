38d1352cf36dc7e22533af6739fb60bd14257e97
Rafael Soares <r.soares2015@outlook.com>
2023-04-06 20:50:55 -0300

    feat: Criacao do componente Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
   return (
     <header>
-        <nav>
-            <div>{'<Rafael />'}</div>
-            <div>
-                <button type='button'>Pagina Inicial</button>
-                <button type='button'>Sobre Mim</button>
-                <button type='button'>Projetos</button>
-            </div>
-        </nav>
+      <nav>
+        <div>{'<Rafael />'}</div>
+        <div>
+          <button type="button">Pagina Inicial</button>
+          <button type="button">Sobre Mim</button>
+          <button type="button">Projetos</button>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
