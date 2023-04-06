2fd05010e4cbcbc5f84c1645680c8d3012ec0fb6
Rafael Soares <r.soares2015@outlook.com>
2023-04-06 20:52:52 -0300

    feat: Tornando o codigo semantico

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <section className="App">
       <Header />
       <h1>Start</h1>
-    </div>
+    </section>
   );
 }
 
