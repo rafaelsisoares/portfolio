9ee2c5f4d73d11fe8b56b4a4f8e36972283401d4
Rafael Soares <r.soares2015@outlook.com>
2023-04-06 20:49:46 -0300

    chore: Configuracao do eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+import Header from './Components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Start</h1>
     </div>
   );
