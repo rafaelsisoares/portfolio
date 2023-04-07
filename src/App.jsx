02be537cf24bde9a446a1fc1363ca47f7914157a
Rafael Soares <r.soares2015@outlook.com>
2023-04-06 22:08:00 -0300

    feat: Criacao da base da pagina About

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
+import About from './Pages/About';
 
 function App() {
   return (
     <section className="App">
       <Routes>
-        <Route exact path="/" Component={Home} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </section>
   );
