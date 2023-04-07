5db4893cffee2ba3ce636cc5267f708bef6f816e
Rafael Soares <r.soares2015@outlook.com>
2023-04-06 22:21:16 -0300

    feat: Criacao da base da pagina Projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import About from './Pages/About';
+import Projects from './Pages/Projects';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </section>
   );
---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../Components/Header';
+
+export default function Projects() {
+  return (
+    <section>
+      <Header />
+      <h1>Veja alguns dos meus projetos</h1>
+    </section>
+  );
+}
