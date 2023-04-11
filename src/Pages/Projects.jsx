e6591fdc4cbb2e707ad9ecd331e9bb4ba9f7e331
Rafael Soares <r.soares2015@outlook.com>
2023-04-11 00:30:35 -0300

    feat: Adicao do projeto de olho no tempo

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,6 +6,7 @@ export default function Projects() {
     <section>
       <Header />
       <h1>Veja alguns dos meus projetos</h1>
+      <hr />
       <div>
         <img src="images/trybeFCPortfolio.png" alt="trybe futebol clube" />
         <p>Projeto Trybe Futebol Clube</p>
@@ -14,6 +15,11 @@ export default function Projects() {
       <div>
         <img src="images/trybetunes.png" alt="trybetunes" />
         <p>Projeto TrybeTunes</p>
+      </div>
+      <br />
+      <div>
+        <img src="images/de-olho-no-tempo.png" alt="de olho no tempo" />
+        <p>Projeto De Olho No Tempo</p>
       </div>
     </section>
   );
