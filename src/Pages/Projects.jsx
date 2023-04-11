bb1639b0bc039c6d5583a6316649ea1fe9a949ee
Rafael Soares <r.soares2015@outlook.com>
2023-04-11 00:08:10 -0300

    feat: Adicao do projeto trybe futebol clube

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,6 +6,10 @@ export default function Projects() {
     <section>
       <Header />
       <h1>Veja alguns dos meus projetos</h1>
+      <div>
+        <img src="../../images/trybeFCPortfolio.png" alt="trybe futebol clube" />
+        <p>Projeto Trybe Futebol Clube</p>
+      </div>
     </section>
   );
 }
