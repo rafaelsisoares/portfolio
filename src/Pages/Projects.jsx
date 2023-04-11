a869f3f45b64dc9f598863236a436094ae3ff704
Rafael Soares <r.soares2015@outlook.com>
2023-04-11 19:28:24 -0300

    feat: Resumo e links adicionados

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 
 export default function Projects() {
@@ -9,17 +10,36 @@ export default function Projects() {
       <hr />
       <div>
         <img src="images/trybeFCPortfolio.png" alt="trybe futebol clube" />
-        <p>Projeto Trybe Futebol Clube</p>
+        <strong>Projeto Trybe Futebol Clube</strong>
+        <p>
+          Projeto full-stack de gerenciamento de um campeonato de futebol. O principal desafio
+          foi desenvolver um back-end funcional para integrá-lo ao front-end já pronto.
+        </p>
+        <Link to="https://github.com/rafaelsisoares/trybe-futebol-clube">Repositório</Link>
       </div>
       <br />
       <div>
         <img src="images/trybetunes.png" alt="trybetunes" />
-        <p>Projeto TrybeTunes</p>
+        <strong>Projeto TrybeTunes</strong>
+        <p>
+          Projeto front-end que simula uma aplicação de streaming de música, nesse projeto
+          foi usada a API do iTunes
+        </p>
+        <Link to="https://github.com/rafaelsisoares/project-trybetunes">Repositório</Link>
+        <br />
+        <Link to="https://trybetunes.bohr.io/">Deploy</Link>
       </div>
       <br />
       <div>
         <img src="images/de-olho-no-tempo.png" alt="de olho no tempo" />
-        <p>Projeto De Olho No Tempo</p>
+        <strong>Projeto De Olho No Tempo</strong>
+        <p>
+          Projeto front-end que exibe informações do tempo em uma cidade, nesse projeto
+          foi usada a API do OpenWeatherMap.
+        </p>
+        <Link to="https://github.com/rafaelsisoares/de-olho-no-tempo">Repositório</Link>
+        <br />
+        <Link to="https://deolhonotempo.bohr.io/">Deploy</Link>
       </div>
     </section>
   );
