844601bef2a03e9be0140f9246fb667e47a58972
Rafael Soares <r.soares2015@outlook.com>
2023-04-14 01:37:19 -0300

    style: Iniciada estilizacao na pagina sobre mim

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -13,6 +13,7 @@ import {
 import { DiMysql, DiMongodb } from 'react-icons/di';
 import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import '../Styles/About.css';
 
 export default function About() {
   return (
@@ -20,55 +21,63 @@ export default function About() {
       <Header />
       <h1>Sobre Mim</h1>
       <section className="about-me">
-        <div>
-          <img src="./images" alt="Foto" />
+        <div className="history">
+          <div className="image">
+            <img src="./images" alt="Foto" />
+          </div>
+          <article>
+            <p>
+              Olá, meu nome é Rafael, sou de São Paulo-SP e a minha paixão pela tecnologia
+              começou quando eu ganhei meu primeiro computador. Naquela época, eu não tinha
+              acesso a Internet, então eu costumava usá-lo como entretenimento, mas sempre tive
+              curiosidade de saber mais sobre o computador, então começei a fazer pequenos cursos
+              relacionados a informática.
+            </p>
+          </article>
         </div>
-        <article>
-          <p>
-            Olá, meu nome é Rafael, sou de São Paulo-SP e a minha paixão pela tecnologia
-            começou quando eu ganhei meu primeiro computador. Naquela época, eu não tinha
-            acesso a Internet, então eu costumava usá-lo como entretenimento, mas sempre tive
-            curiosidade de saber mais sobre o computador, então começei a fazer pequenos cursos
-            relacionados a informática.
-          </p>
-        </article>
-        <div>
-          <img src="./images" alt="Foto" />
+        <div className="history">
+          <div className="image">
+            <img src="./images" alt="Foto" />
+          </div>
+          <article>
+            <p>
+              Meu primeiro contato com a programação foi em um curso técnico de desenvolvimento
+              de jogos. Nesse curso aprendi o básico de HTML, CSS, C++, Java, Banco de dados
+              relacional e Game Maker. O meu último projeto neste curso foi desenvolver um jogo,
+              desenvolvi o Airplane vs UFO, um space shooter 2D onde o jogador controla uma nave
+              e precisa destruir os inimigos (discos voadores) além de desviar dos asteróides para
+              enfim derrotar a nave mãe.
+              E foi nesse curso que o meu interesse pela programação floriu.
+            </p>
+          </article>
         </div>
-        <article>
-          <p>
-            Meu primeiro contato com a programação foi em um curso técnico de desenvolvimento
-            de jogos. Nesse curso aprendi o básico de HTML, CSS, C++, Java, Banco de dados
-            relacional e Game Maker. O meu último projeto neste curso foi desenvolver um jogo,
-            desenvolvi o Airplane vs UFO, um space shooter 2D onde o jogador controla uma nave
-            e precisa destruir os inimigos (discos voadores) além de desviar dos asteróides para
-            enfim derrotar a nave mãe.
-            E foi nesse curso que o meu interesse pela programação floriu.
-          </p>
-        </article>
-        <div>
-          <img src="./images" alt="Foto" />
+        <div className="history">
+          <div className="image">
+            <img src="./images" alt="Foto" />
+          </div>
+          <article>
+            <p>
+              Entrei no ensino superior, porém cursei tecnologia em redes de computadores.
+              Nesta graduação, aprendi como os computadores se comunicam, os protocolos usados,
+              configurar roteador e switch, fazer endereçamento IPv4 e IPv6, entre outros conteúdos.
+            </p>
+          </article>
         </div>
-        <article>
-          <p>
-            Entrei no ensino superior, porém cursei tecnologia em redes de computadores.
-            Nesta graduação, aprendi como os computadores se comunicam, os protocolos usados,
-            configurar roteador e switch, fazer endereçamento IPv4 e IPv6, entre outros conteúdos.
-          </p>
-        </article>
-        <div>
-          <img src="./images" alt="Foto" />
+        <div className="history">
+          <div className="image">
+            <img src="./images" alt="Foto" />
+          </div>
+          <article>
+            <p>
+              Em 2022, enquanto trabalhava com meu pai na lanchonete dele, conheci a Trybe
+              e decidi dar uma chance ao curso já que eu não estava satisfeito com o que eu
+              fazia naquela época. Durante o curso, aprendi muito sobre desenvolvimento WEB,
+              me aprofundei em JavaScript, desenvolvimento front-end com React.js, desenvolvimento
+              back-end com Node.js, TypeScript, além de desenvolver dezenas de projetos tanto
+              individuais como em grupo.
+            </p>
+          </article>
         </div>
-        <article>
-          <p>
-            Em 2022, enquanto trabalhava com meu pai na lanchonete dele, conheci a Trybe
-            e decidi dar uma chance ao curso já que eu não estava satisfeito com o que eu
-            fazia naquela época. Durante o curso, aprendi muito sobre desenvolvimento WEB,
-            me aprofundei em JavaScript, desenvolvimento front-end com React.js, desenvolvimento
-            back-end com Node.js, TypeScript, além de desenvolver dezenas de projetos tanto
-            individuais como em grupo.
-          </p>
-        </article>
       </section>
       <hr />
       <section className="stacks">
@@ -87,13 +96,13 @@ export default function About() {
       <section className="contacts">
         <h1>Meus contatos</h1>
         <Link to="https://www.linkedin.com/in/rafael-soares-dev/">
-          <FaLinkedin />
+          <FaLinkedin className="linkedin" />
         </Link>
         <Link to="https://github.com/rafaelsisoares">
-          <FaGithub />
+          <FaGithub className="github" />
         </Link>
         <Link to="mailto:r.soares2015@outlook.com">
-          <FaEnvelope />
+          <FaEnvelope className="email" />
         </Link>
       </section>
     </section>
