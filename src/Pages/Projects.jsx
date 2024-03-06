cda76b8d813e5cf8cc68e8c246faa3f0ae5ad363
Rafael Soares <r.soares2015@outlook.com>
2024-03-06 10:37:15 -0300

    feat: Adicao do projeto MyBook

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -89,6 +89,24 @@ export default function Projects() {
             </Link>
           </div>
         </div>
+        <div className="project">
+          <img src="images/mybookCapa.png" alt="mybook" />
+          <br />
+          <strong>Projeto MyBook</strong>
+          <p>
+            Projeto pessoal feito em React que recria uma rede social
+            onde é possível ver e criar postagens, curtir e comentar nas postagens existentes.
+            Embora o projeto tenha apenas o front-end, há validação de usuário e senha.
+          </p>
+          <div className="links">
+            <Link to="https://github.com/rafaelsisoares/mybook">
+              <button type="button" className="btn-repo">Repositório</button>
+            </Link>
+            <Link to="https://mybook.bohr.io/">
+              <button type="button" className="btn-deploy">Deploy</button>
+            </Link>
+          </div>
+        </div>
       </div>
       <Footer />
     </section>
