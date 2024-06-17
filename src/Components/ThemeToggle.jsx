b17bd78dd0541f8dd493316eae6c69930d0c7cac
Rafael Soares <r.soares2015@outlook.com>
2024-06-17 18:23:41 -0300

    feat: Inicio da implementação do interruptor para troca de tema

---
+++ src/Components/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Styles/ThemeToggle.css';
+
+export default function ThemeToggle() {
+  return (
+    <div className="toggle-bk">
+      <div className="toggle" />
+    </div>
+  );
+}
